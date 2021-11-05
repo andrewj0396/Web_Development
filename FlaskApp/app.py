9957d5c87c6141d32bbeecef2783dbc0e832bb6d
BlueSquare23 <johnlradford23@gmail.com>
2021-11-05 11:42:05 -0400

    Updated app.py to removed unneeded code.

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,11 +1,4 @@
 from website import run_app
-import platform
-
-HOSTNAME = platform.uname().node
-
-# Set's hostname (useful for development)
-if HOSTNAME != "wopaguz.com" and HOSTNAME != "vps2754.pairvps.com":
-	HOSTNAME = "localhost"
 
 if __name__ == "__main__":
 	app = run_app()
